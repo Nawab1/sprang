d66025a9abf1e4df2036186e92802f7639572be8
kali <kali@DESKTOP-TH3UNTV>
2021-06-20 14:18:21 +0530

    repository added

--- springSecuDur/src/main/java/com/example/demo/model/Role.java
+++ springSecuDur/src/main/java/com/example/demo/model/Role.java
@@ -9,7 +9,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
+import lombok.Data;
+
 @Entity
+@Data
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- springSecuDur/src/main/java/com/example/demo/model/User.java
+++ springSecuDur/src/main/java/com/example/demo/model/User.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import lombok.Data;
+
 @Entity
+@Data
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
  private Long id;
  private String userName;
  private String firstName;
